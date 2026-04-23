fa8ee0582d3758d9bba6e103d7215f0712a524f1
haeun228 <123haeun@naver.com>
2026-04-23 17:56:21 +0900

    feat: Inbox 엔티티 & repository 구현

---
+++ src/main/java/com/firstticket/common/messaging/inbox/InboxRepository.java
@@ -0,0 +1,10 @@
+package com.firstticket.common.messaging.inbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface InboxRepository extends JpaRepository<Inbox, UUID> {
+
+    boolean existsById(UUID id);
+}
